48278a3bbe1c9f32e059cdc9c3769f981ee67f2b
Erik <szarkaerik18@gmail.com>
2026-05-24 16:51:54 +0200

    login letrehozas

---
+++ Szemelyes_Penzugyi_Nyilvantarto/login.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Szemelyes_Penzugyi_Nyilvantarto
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Password;
+
+            if(username == "admin" && password == "admin")
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Hibás felhasználónév vagy jelszó!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+        }
+    }
+}
