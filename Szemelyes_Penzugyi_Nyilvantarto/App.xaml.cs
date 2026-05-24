f6627c3ef35e006245cf09823976acf30c1584e2
Marci <Marci@ERIK>
2026-05-24 15:28:27 +0200

    Add project files.

---
+++ Szemelyes_Penzugyi_Nyilvantarto/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Szemelyes_Penzugyi_Nyilvantarto
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
